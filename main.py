60a478d30360c867454d4ec897bff875a49bd958
- <->
2023-02-27 17:06:23 +0500

    hello

--- main.py
+++ main.py
@@ -36,9 +36,9 @@ def run_training(datamodule):
         logger=[tb_logger],
         num_sanity_val_steps=0,
         enable_model_summary=False,
-        # enable_checkpointing=False,
+        enable_checkpointing=False,
         log_every_n_steps=1,
-        callbacks=[checkpoint_callback],
+        # callbacks=[checkpoint_callback],
         # fast_dev_run=True
     )
     module = LitResnet(0.02, 'Adam', num_classes=10)
