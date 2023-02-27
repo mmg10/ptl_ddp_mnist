0fa4fdd83de154a65ce42cabc1054e5a8191b795
- <->
2023-02-27 17:21:32 +0500

    hello

--- model.py
+++ model.py
@@ -142,7 +142,7 @@ class LitResnet(pl.LightningModule):
         self.logger.experiment.add_scalar("train_acc_epoch_manual",
                                             acc,
                                             self.current_epoch)
-        self.log('train_acc_epoch_self', acc, sync_dist=True)
+        self.log('train_acc_epoch_self', acc, sync_dist=True, logger=False)
 
     def configure_optimizers(self):
         
