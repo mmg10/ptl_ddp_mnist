344aca4cee87922c34cf4b9d9b6aed3b08e35a98
- <->
2023-03-01 15:45:46 +0500

    fsdp

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def run_training(datamodule):
     trainer = pl.Trainer(
         max_epochs=EPOCHS,
         accelerator=DEVICE,
-        strategy='fsdp_native',
+        strategy='fsdp',
         devices=NUM_DEVICES,
         num_nodes=2,
         # num_nodes=WORLD_SIZE,
